8eb1c3b1d833f1d95d2a3056e9673709c5b14483
chenjiancong <chenjiancong@sutpc.com>
2025-03-05 10:34:55 +0800

    部署更新配置

---
+++ config/.env.pro
@@ -0,0 +1,7 @@
+NODE_ENV = production
+
+# 打包路径
+VITE_BASE_PATH = /
+
+# 应用名称，用于网页头部等
+VITE_APP_TITLE = ai-music
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,54 @@
-import { fileURLToPath, URL } from 'node:url'
+import { loadEnv } from 'vite';
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
+import type { UserConfig, ConfigEnv } from 'vite';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+const root = process.cwd();
+const pathResolve = (dir: string) => resolve(root, '.', dir);
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.less', '.css', 'vue'],
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  const isBuild = command === 'build';
+  const argMode = process.argv[3] === '--mode' ? process.argv[4] : process.argv[3];
+  const configDir = pathResolve('config');
+  const env = loadEnv(isBuild ? mode : argMode, configDir);
+
+  return {
+    envDir: pathResolve('config'),
+    // base: env.VITE_BASE_PATH,
+    base: '/app/', // 关键配置！所有资源路径添加 /app/ 前缀
+    build: {
+      cssCodeSplit: true, // 启用 CSS 分包
+      outDir: 'dist', // 输出目录为 dist（无需修改）
+      target: 'esnext', //browsers can handle the latest ES features
+      rollupOptions: {
+        input: {
+          index: './index.html'
+        },
+        output: {
+          manualChunks: (id) => {
+            // 将 node_modules 中的依赖打包到 vendor.js
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          // // 输出文件名格式（支持哈希）
+          // entryFileNames: '[hash].js',
+          // chunkFileNames: '[hash].js',
+          // assetFileNames: '[hash].[ext]'
+        },
+      }
     },
-  },
-})
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.less', '.css', 'vue'],
+      alias: [
+        {
+          find: /\@\//,
+          replacement: `${pathResolve('src')}/`
+        }
+      ]
+    },
+  };
+};
